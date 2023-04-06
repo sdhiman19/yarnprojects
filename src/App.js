fc26f63bdc42144f7bee3d73fb04f1251ff727a9
sdhiman19 <sumitdhiman190@gmail.com>
2023-04-06 15:16:56 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+App.js
+import React from 'react';
+import Navbar from './Navbar';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Simple Navbar</h1>
+    </div>
+  );
+}
+
+export default App;
